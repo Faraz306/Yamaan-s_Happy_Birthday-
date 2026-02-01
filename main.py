fa7994e674ce5cf5b4044baca43901899c533549
Faraz <private.1984@gmail.com>
2026-02-01 15:08:01 +0000

    added ballons

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ no = st.button("No")
 # Fix: Correct name check
 if input_name.strip() in ["Yamaan", "Yamaan Faraz"]:
     st.success("You are right! It is Yamaan Faraz's birthday!")
+    st.balloons()
 
     # Ask about guests
     st.write(f"If you want to know who will come in {input_name}'s birthday, press Yes, otherwise press No.")
@@ -22,5 +23,6 @@ if input_name.strip() in ["Yamaan", "Yamaan Faraz"]:
     if yes2:
         cats = ["Jack", "Pinu", "Chinu", "Cacto", "Kalia"]
         st.success(f"{', '.join(cats)} will come in {input_name}'s birthday!")
+        st.balloons()
     elif no2:
         st.write("Okay, we will not tell the name.")
